f6cf6fdf21744d95685d4a82a0c49105fb417213
KrzysztofSolowiej <krsolowiej@protonmail.com>
2023-01-10 20:27:43 +0100

    final

--- Fire_vs_Weather_Project/App.py
+++ Fire_vs_Weather_Project/App.py
@@ -1,5 +1,5 @@
 import joblib
-from flask import Flask, request, escape
+from flask import Flask, request, render_template
 import numpy as np
 
 app = Flask(__name__)
@@ -15,25 +15,8 @@ def upload_data():
     if request.method == 'POST':
         avr_temp_C = request.values['avr_temp_C']    
         label_pred = get_pred(np.array(avr_temp_C, dtype=float))
-        return '''
-        <!doctype html>
-        <title>Simple prediction model based on linear regression algorithm</title>
-        <h1>Simple prediction model based on linear regression algorithm</h1>
-        <p>Prediction results</p> 
-        <p>Average monthly temperature: {}</p>
-        <p>Estimated number of fire department deployments: {} </p>
-        '''.format(escape(avr_temp_C), escape(np.round(np.exp(label_pred))))
+        return render_template('index.html', temp=avr_temp_C, result=np.round(np.exp(label_pred)))
+    return render_template('home.html')
 
-        
-    return '''
-    <!doctype html>
-    <title>Simple prediction model based on linear regression algorithm</title>
-    <h1>Simple prediction model based on linear regression algorithm</h1>
-    <p>Enter the average monthly temperature</p>
-    <form method=post enctype=multipart/form-data>
-      <p><input type="number" name="avr_temp_C">
-         <input type=submit value="OK">
-    </form>
-    '''
 if __name__ == "__main__":
     app.run(debug=True)
